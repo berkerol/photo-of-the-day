b1779b90a0e67c7b32673793aa1f755df9c3b0be
Berk Erol <berk.erol@boun.edu.tr>
2017-06-08 02:12:40 +0300

    saves photos & cleanup

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Photo of the Day
 
-Downloads Bing Photo of the Day and makes it desktop background (for Linux GNOME desktop)
+* Downloads Bing Photo of the Day and makes it desktop background (for Linux GNOME desktop)
+* Saves all photos to ~/Pictures/Bing
 
 ## TODO
 
--- bing.py
+++ bing.py
@@ -2,11 +2,8 @@ import json
 from subprocess import check_output
 from gi.repository import Gio
 
-check_output("curl -X GET 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US' > file.json", shell = True)
-f = open("file.json", "r")
-url = json.load(f)['images'][0]['url']
-f.close()
-check_output("rm file.json", shell = True)
-check_output("wget https://bing.com" + url, shell = True)
+url = json.loads(check_output("curl -X GET 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'", shell = True))['images'][0]['url']
+filename = "/home/" + check_output("whoami", shell = True).split()[0] + "/Pictures/Bing/" + url[url.rfind("/") + 1:]
+check_output("wget -xO " + filename + " https://bing.com" + url, shell = True)
 gsettings = Gio.Settings.new('org.gnome.desktop.background')
-gsettings.set_string('picture-uri', 'file://%s' % (check_output("pwd", shell = True).split()[0] + "/" + url[url.rfind("/") + 1:]))
+gsettings.set_string('picture-uri', 'file://%s' % filename)
