89a65562a6d1763bbbc33890db80e0a47c4fd87b
Berk Erol <berk.erol@boun.edu.tr>
2017-06-08 22:49:14 +0300

    mac try 2

--- bing.py
+++ bing.py
@@ -8,11 +8,11 @@ elif platform == "darwin":
     mode = False
 else:
     sys.exit("not supported yet")
-url = json.loads(check_output("curl -X GET 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'", shell = True))['images'][0]['url']
+url = json.loads(check_output("curl -X GET 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'", shell = True).decode('utf-8'))['images'][0]['url']
 if mode:
     dirname = "/home/" + check_output("whoami", shell = True).split()[0] + "/Pictures/Bing/"
 else:
-    dirname = "/Users/" + check_output("whoami", shell = True).split()[0] + "/Pictures/Bing/"
+    dirname = "/Users/" + check_output("whoami", shell = True).decode('utf-8').split()[0] + "/Pictures/Bing/"
 filename = dirname + url[url.rfind("/") + 1:]
 check_output("mkdir -p " + dirname + " && wget https://bing.com" + url + " -O " + filename, shell = True)
 if mode:
