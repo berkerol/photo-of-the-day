ee78316e306970f68f70140b6365c7b958b3f356
Berk Erol <berk.erol@boun.edu.tr>
2017-06-08 20:32:43 +0300

    wget fix

--- bing.py
+++ bing.py
@@ -3,7 +3,8 @@ from subprocess import check_output
 from gi.repository import Gio
 
 url = json.loads(check_output("curl -X GET 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'", shell = True))['images'][0]['url']
-filename = "/home/" + check_output("whoami", shell = True).split()[0] + "/Pictures/Bing/" + url[url.rfind("/") + 1:]
-check_output("wget -xO " + filename + " https://bing.com" + url, shell = True)
+dirname = "/home/" + check_output("whoami", shell = True).split()[0] + "/Pictures/Bing/"
+filename = dirname + url[url.rfind("/") + 1:]
+check_output("mkdir -p " + dirname + " && wget https://bing.com" + url + " -O " + filename, shell = True)
 gsettings = Gio.Settings.new('org.gnome.desktop.background')
 gsettings.set_string('picture-uri', 'file://%s' % filename)
