15cbee98f14fda2205de1723a58a832e94406051
Berk Erol <berk.erol@boun.edu.tr>
2017-06-11 10:53:45 +0300

    small fixes & website

--- potd.py
+++ potd.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-import sys, json, argparse
+import sys, json, time, argparse
 from sys import platform
-from datetime import datetime
 from subprocess import check_output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nasa', dest='type', action='store_false', default = True)
-    parser.add_argument('-d', '--date', dest='date', action='store', default=datetime.now().strftime("%Y-%m-%d"), type=str)
-    parser.add_argument('-l', '--locale', dest='locale', action='store', default='en-US', type=str)
-    parser.add_argument('-r', '--resolution', dest='resolution', action='store', default='1920x1080', type=str)
+    parser.add_argument('-n', '--nasa', dest='type', action='store_false', default = True, help='NASA photo option')
+    parser.add_argument('-l', '--locale', dest='locale', action='store', default='en-US', type=str, help='Bing locale option')
+    parser.add_argument('-r', '--resolution', dest='resolution', action='store', default='1920x1080', type=str, help='Bing resolution option')
+    parser.add_argument('-d', '--date', dest='date', action='store', default=time.strftime("%Y-%m-%d"), type=str, help='NASA date option')
     arguments = parser.parse_args()
     if platform == "linux" or platform == "linux2":
         mode = True
@@ -20,7 +19,7 @@ if __name__ == "__main__":
         sys.exit("not supported yet")
     if arguments.type:
         photo = json.loads(check_output("curl -X GET 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=%s'" % arguments.locale, shell = True).decode('utf-8'))['images'][0]['url']
-        url = "https://bing.com" + photo[:photo.rfind("_") + 1] + arguments.resolution + ".jpg"
+        url = "https://www.bing.com" + photo[:photo.rfind("_") + 1] + arguments.resolution + ".jpg"
         storedir = "/Pictures/Bing/"
     else:
         url = json.loads(check_output("curl -X GET 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&hd=True&date=%s'" % arguments.date, shell = True).decode('utf-8'))['hdurl']
